6e65124e9c782ac55a55252c73db3bf97df28d22
Joe Wu <wubaixing1379@163.com>
2017-02-24 01:46:06 +0800

    finish

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ $(function(){
         anchors: ['page1','page2','page3','page4'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['图表一','图表二','','图表三'],
+        navigationTooltips: ['图表一','图表二','图表三','图表四'],
         showActiveTooltip: true
     });
 
@@ -36,21 +36,32 @@ $(function(){
             backgroundColor : '#dbe9d8',
         },
         title: {
-            text: '深圳二月份历史均值气温'
+            text: '深圳二月份历史均值气温',
+            style: {
+                fontSize: '20px',
+                fontWeight: 'bold'
+            }
         },
         subtitle: {
             text: '2017/2/23/16:05'
         },
         xAxis: {
+            title: {
+                text: '日期'
+            },
             type: 'datetime',
             labels: {
                 overflow: 'justify'
-            }
+            },
+            lineWidth: 3,
+            lineColor: '#afe5a2'
         },
         yAxis: {
             title: {
                 text: '摄氏温度(℃)'
-            }
+            },
+            lineWidth: 3,
+            lineColor: '#afe5a2'
         },
         tooltip: {
             valueSuffix: '℃'
@@ -78,4 +89,170 @@ $(function(){
             data: [15,16,15,16,17,17,17,14,13,13,14,11,10,12,13,12,13,13,10,10,13,15,16,17,18,17,18,17]
         }]
     })
+
+    $('#chart2').highcharts({
+        chart: {
+            type: 'bar',
+            backgroundColor : '#C2D4D8',
+        },
+        title: {
+            text: '网站用户访问来源统计',
+            style: {
+                fontSize: '20px',
+                fontWeight: 'bold'
+            }
+        },
+        subtitle: {
+            text: '数据纯属虚构'
+        },
+        xAxis: {
+            categories: ['周一','周二','周三','周四','周五','周六','周日'],
+            lineWidth: 3,
+            lineColor: '#68c3d6'
+        },
+        yAxis: {
+            title: {
+                text: '访问来源数量'
+            },
+            lineWidth: 3,
+            lineColor: '#68c3d6',
+            gridLineWidth: 0
+        },
+        legend: {
+            reverse: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            name: '直接访问',
+            data: [320, 302, 301, 334, 390, 330, 320]
+        },{
+            name: '邮件营销',
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },{
+            name: '联盟广告',
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },{
+            name: '视频广告',
+            data: [150, 212, 201, 154, 190, 330, 410]
+        },{
+            name: '搜索引擎',
+            data: [820, 832, 901, 934, 1290, 1330, 1320]
+        }]
+    })
+
+    $('#chart3').highcharts({
+        chart: {
+            type: 'pie',
+            backgroundColor : '#F2EFE8',
+        },
+        title: {
+            text: '网站用户访问来源统计',
+            style: {
+                fontSize: '20px',
+                fontWeight: 'bold'
+            }
+        },
+        subtitle: {
+            text: '数据纯属虚构'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true
+            }
+        },
+        series: [{
+            name: '所占比例',
+            colorByPoint: true,
+            data: [{
+                name: '直接访问',
+                y: 335
+            },{
+                name: '邮件营销',
+                y: 310
+            },{
+                name: '联盟广告',
+                y: 234
+            },{
+                name: '视频广告',
+                y: 135
+            },{
+                name: '搜索引擎',
+                y: 1548
+            }]
+        }]
+    })
+
+    var chart = new Highcharts.Chart({
+        chart: {
+            renderTo: 'chart4',
+            type: 'column',
+            backgroundColor : '#B0ABC2',
+            options3d: {
+                enabled: true,
+                alpha: 15,
+                beta: 15,
+                depth: 50,
+                viewDistance: 25
+            },
+        },
+        title: {
+            text: '2016中国手机销量排行榜',
+            style: {
+                fontSize: '20px',
+                fontWeight: 'bold'
+            }
+        },
+        subtitle: {
+            text: 'OPPO暴增,小米跌惨！'
+        },
+        xAxis: {
+            categories: ['华为','OPPO','vivo','小米','中兴','联想','TCL','金立','魅族','乐视'],
+            lineWidth: 3,
+            lineColor: '#715bbf',
+            // gridLineColor: '#197F07',//纵向网格线颜色
+            gridLineWidth: 0 //纵向网格线宽度
+        },
+        yAxis: {
+            title: {
+                text: '销量（百万台）'
+            },
+            lineWidth: 3,
+            lineColor: '#715bbf',
+            gridLineColor: '#715bbf'
+        },
+        tooltip: {
+            valueSuffix: '百万台'
+        },
+         plotOptions: {
+            column: {
+                depth: 25
+            }
+        },
+        series: [{
+            name: '手机厂商',
+            data: [139,95,82,58,57,50,34,28,22,19]       
+        }]
+    })
+
+    function showValues() {
+        $('#alpha-value').html(chart.options.chart.options3d.alpha);
+        $('#beta-value').html(chart.options.chart.options3d.beta);
+        $('#depth-value').html(chart.options.chart.options3d.depth);
+    }
+
+    // Activate the sliders
+    $('#sliders input').on('input change', function () {
+        chart.options.chart.options3d[this.id] = this.value;
+        showValues();
+        chart.redraw(false);
+    });
+
+    showValues();
 })
